fa8d0058146a9d9a585b411fe11eb997aea1cf62
Mirko Jahn <mirkojahn@gmail.com>
2010-04-15 03:23:10 +0200

    initial commit of the memory agent stub

---
+++ mem.agent/src/test/java/net/mjahn/agent/inspector/LibTester.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.mjahn.agent.inspector;
+
+/**
+ *
+ * @author mjahn
+ */
+public class LibTester {
+
+    public static void printerTest(){
+        System.out.println("Test successful!");
+    }
+
+}
