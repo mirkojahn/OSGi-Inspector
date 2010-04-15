a3c0671432d6810de393ef1c22d89def470b9d84
Mirko Jahn <mirkojahn@gmail.com>
2010-04-15 03:32:36 +0200

    renamed the core agent library

---
+++ mem.agent/src/main/java/net/mjahn/mem/agent/MemAgent.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.mjahn.mem.agent;
+
+/**
+ *
+ * @author mjahn
+ */
+public class MemAgent {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Loading VM MemAgent.");
+        MemAgent insp = new MemAgent();
+        System.out.println("This agent used "
+                + insp.getSizeOfAllLoadedObjectsBy(
+                        insp.getClass().getClassLoader())
+                + " bytes of memory.");
+    }
+
+    public native long getSizeOfAllLoadedObjectsBy(ClassLoader cl);
+
+    public native Object[] getAllLoadedObjectsOf(ClassLoader cl);
+
+    public native Object[] getAllClassLoadersOfType(String classLoaderName);
+
+    public native ClassLoader[] getAllClassLoaders();
+    
+}
